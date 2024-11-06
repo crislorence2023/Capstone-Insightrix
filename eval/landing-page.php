8e9f46a46bdf22dc8120f24923a9761b3c2f279e
crislorence2023 <crislorence2020@gmail.com>
2024-11-06 23:15:46 +0800

    Update landing-page.php

--- eval/landing-page.php
+++ eval/landing-page.php
@@ -310,7 +310,7 @@
         <div class="nav-container">
             <a href="#" class="logo">
                 <i class="fas fa-chart-line"></i>
-                Insightrix(BETA2)
+                Insightrix(BETA)
             </a>
             <a href="Assessmentsurvey/login.php" class="btn btn-primary">
                 <i class="fas fa-sign-in-alt"></i>
