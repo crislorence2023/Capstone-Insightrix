4e9c5864f1040d2e2065d2d59ae17abe43bf68cf
crislorence2023 <crislorence2020@gmail.com>
2024-11-11 23:02:40 +0800

    Update landing-page.php

--- eval/landing-page.php
+++ eval/landing-page.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insightrix - Teacher Assessment System</title>
+    <link rel="icon" href="logo/evalucator-nobg2.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
