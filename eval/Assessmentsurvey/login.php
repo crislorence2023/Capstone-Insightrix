57fd585023a3fef6541923ab897c0ec7cb26322a
crislorence2023 <crislorence2020@gmail.com>
2024-12-12 00:35:43 +0800

    Update login.php

--- eval/Assessmentsurvey/login.php
+++ eval/Assessmentsurvey/login.php
@@ -537,7 +537,7 @@ input-group1 {
                 </div>
             </div>
 
-            <a href="forgot-password.php" class="forgot-password">Forgot Password?</a>
+            <!-- <a href="forgot-password.php" class="forgot-password">Forgot Password?</a> -->
             
             <div class="input-group1">
                 <div class="dropdown">
